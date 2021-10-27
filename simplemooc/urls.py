943373f99e5aba6d9bdf7820de227a95200bcd27
Thiago <thiagolima1011@gmail.com>
2021-10-26 21:17:01 -0300

    bugfix do site simplemooc pagina inicial - home page

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.hello),
+    path('', views.hello),
     path('contato/', views.contact)
 ]
